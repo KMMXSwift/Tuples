64d13ed0afa5a042b2d8430edfa69956a4f125cd
Ricardo Michel Reyes <me@richiereyes.com>
2016-03-05 12:41:51 -0600

    Initial Commit

---
+++ TupleTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TupleTable
+//
+//  Created by Ricardo Michel Reyes Martínez on 3/5/16.
+//  Copyright © 2016 Ediciones Internacionales BCA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
